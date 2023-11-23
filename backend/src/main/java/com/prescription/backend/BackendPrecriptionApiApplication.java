9d612b9fb0aa79d2153b7d56116b0e2cf742dd9d
Bartek <ostrycharz.bartlomiej@gmail.com>
2023-11-23 16:32:57 +0100

    Initialized spring project

---
+++ backend/src/main/java/com/prescription/backend/BackendPrecriptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.prescription.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPrecriptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPrecriptionApiApplication.class, args);
+	}
+
+}
