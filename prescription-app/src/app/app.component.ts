be2ad48754918faf0abf2867f452ce640939c289
Bartek <ostrycharz.bartlomiej@gmail.com>
2023-12-05 18:50:55 +0100

    Created navigation bar and component with input

--- prescription-app/src/app/app.component.ts
+++ prescription-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationBarComponent, InputFieldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
