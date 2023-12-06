429f96febf434fdf42a427cea7bed57d26a3a11f
Adam Walkowiak <adam.walkowiak028@gmail.com>
2023-12-06 22:33:04 +0100

    updating the patient controller

--- backend/src/main/java/com/prescription/backend/Controller/PatientController.java
+++ backend/src/main/java/com/prescription/backend/Controller/PatientController.java
@@ -4,6 +4,7 @@ import com.prescription.backend.Model.Patient;
 import com.prescription.backend.Service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
