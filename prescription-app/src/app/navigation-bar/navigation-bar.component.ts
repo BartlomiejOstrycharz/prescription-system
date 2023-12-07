036bcd623ea1f2e7bf4b2de8313db24f8dd9f29c
Bartek <ostrycharz.bartlomiej@gmail.com>
2023-12-07 21:47:07 +0100

    Created routes and exit button

--- prescription-app/src/app/navigation-bar/navigation-bar.component.ts
+++ prescription-app/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.css'
+  styleUrl: './navigation-bar.component.css',
 })
 export class NavigationBarComponent {
+  showButton: boolean = false;
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showButton = this.router.url === '/prescription-table';
+      }
+    });
+  }
 }
