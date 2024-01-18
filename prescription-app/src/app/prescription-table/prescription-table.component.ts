8a824beeb16fb13b71238518af88f60ff8a0814a
Bartek <ostrycharz.bartlomiej@gmail.com>
2024-01-18 18:52:03 +0100

    Created pdf generation with qr code

--- prescription-app/src/app/prescription-table/prescription-table.component.ts
+++ prescription-app/src/app/prescription-table/prescription-table.component.ts
@@ -5,6 +5,11 @@ import { PrescriptionService } from '../service/prescription/prescription.servic
 import { CommonModule } from '@angular/common';
 import { Prescription } from '../model/Prescription';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import * as QRCode from 'qrcode';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
+
+
 
 @Component({
   selector: 'app-prescription-table',
@@ -86,5 +91,68 @@ export class PrescriptionTableComponent implements OnInit {
         console.error('Error fetching prescription details: ', error);
       }
     );
+  }
+  async generatePDF() {
+    // Get the HTML content you want to print
+    const content = document.getElementById('print-content');
+    const printButton = document.getElementById('print-button');
+
+    // Hide the button temporarily
+    if (printButton) {
+      printButton.style.display = 'none';
+    }
+
+    // Configuration for html2canvas
+    const options = {
+      scale: 4,  // Increase the scale for higher resolution
+      logging: true,
+      allowTaint: true,
+      useCORS: true,
+      scrollX: 0,
+      scrollY: 0,
+      windowWidth: document.documentElement.scrollWidth,
+      windowHeight: document.documentElement.scrollHeight,
+    };
+
+    // Create a new jsPDF instance with landscape orientation
+    const pdf = new jsPDF({
+      orientation: 'landscape',
+      unit: 'mm',
+      format: 'a4',
+    });
+
+    // Use html2canvas to capture the HTML content with improved quality
+    const canvas = await html2canvas(content!, options);
+    const imgData = canvas.toDataURL('image/png');
+
+    // Calculate the aspect ratio to maintain the original proportions
+    const imgWidth = 297;  // Adjust as needed for landscape
+    const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+
+    // Generate a random QR code using the qrcode library
+    const qrCodeData = Math.random().toString(36).substring(2, 15);
+    const qrCodeOptions = {
+      margin: 2,
+      width: 40,
+      color: {
+        dark: '#000000',  // QR code color
+        light: '#ffffff', // Background color
+      },
+    };
+
+    // Convert the QR code data to a data URL
+    const qrCodeDataURL = await QRCode.toDataURL(qrCodeData, qrCodeOptions);
+
+    pdf.addImage(qrCodeDataURL, 'PNG', imgWidth-50, imgHeight + 20, 40, 40);
+
+    // Save or open the PDF
+    pdf.save('prescription.pdf');
+
+    // Show the button again after capturing
+    if (printButton) {
+      printButton.style.display = '';
+    }
   }
 }
